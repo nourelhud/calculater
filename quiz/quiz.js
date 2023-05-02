f908811c1a9726d8b5f5450e93a62be4b05efe58
Nour El-Huda <107063090+nourelhud@users.noreply.github.com>
2023-05-02 15:41:38 +0300

    Add files via upload

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -1,23 +1,3 @@
-// function add() {
-//     let num1 = parseInt(document.getElementById('num1').value);
-//     let num2 = parseInt(document.getElementById('num2').value);
-//     let result = num1 + num2;
-//     document.getElementById('result').innerHTML = "Result: " + result;
-//   }
-  
-//   function subtract() {
-//     let num1 = parseInt(document.getElementById('num1').value);
-//     let num2 = parseInt(document.getElementById('num2').value);
-//     let result = num1 - num2;
-//     document.getElementById('result').innerHTML = "Result: " + result;
-//   }
-  
-//   function clearInputs() {
-//     document.getElementById('num1').value = "";
-//     document.getElementById('num2').value = "";
-//     document.getElementById('result').innerHTML = "";
-//   }
-  
 function add() {
 	var num1 = parseFloat(document.getElementById("num1").value);
 	var num2 = parseFloat(document.getElementById("num2").value);
@@ -35,11 +15,10 @@ function subtract() {
 	document.getElementById("result").innerHTML = result;
     document.getElementById("result").classList.remove("d-none");
 }
-
-  
-  function clearInput() {
+function clearInput() {
     document.getElementById("num1").value = "";
     document.getElementById("num2").value = "";
     document.getElementById("result").innerHTML = "";
-  }
+    document.getElementById("result").classList.add("d-none");
+}
   
